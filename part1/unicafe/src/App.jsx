e13568e1de940f2d5f83531e77d9fec10aed998e
adityackr <aditya09me@gmail.com>
2022-09-07 10:43:20 +0600

    Add: Assignment 1.11

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -2,9 +2,10 @@ import { useState } from 'react';
 
 const StatisticsLine = ({ text, value }) => {
 	return (
-		<p>
-			{text} {value}
-		</p>
+		<tr>
+			<td>{text}</td>
+			<td>{value}</td>
+		</tr>
 	);
 };
 
@@ -17,14 +18,16 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
 		);
 	}
 	return (
-		<div>
-			<StatisticsLine text="good" value={good} />
-			<StatisticsLine text="neutral" value={neutral} />
-			<StatisticsLine text="bad" value={bad} />
-			<StatisticsLine text="all" value={all} />
-			<StatisticsLine text="average" value={average} />
-			<StatisticsLine text="positive" value={positive} />
-		</div>
+		<table>
+			<tbody>
+				<StatisticsLine text="good" value={good} />
+				<StatisticsLine text="neutral" value={neutral} />
+				<StatisticsLine text="bad" value={bad} />
+				<StatisticsLine text="all" value={all} />
+				<StatisticsLine text="average" value={average} />
+				<StatisticsLine text="positive" value={positive} />
+			</tbody>
+		</table>
 	);
 };
 
