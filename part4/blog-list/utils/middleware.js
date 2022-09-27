92f3ba0ededde73f2025e16dbc9896fd37fc88cf
adityackr <aditya09me@gmail.com>
2022-09-27 11:02:58 +0600

    feat: add assignment 4.18-4.20

--- part4/blog-list/utils/middleware.js
+++ part4/blog-list/utils/middleware.js
@@ -11,12 +11,34 @@ const errorHandler = (error, request, response, next) => {
 		return response.status(400).send({ error: 'malformatted id' });
 	} else if (error.name === 'ValidationError') {
 		return response.status(400).json({ error: error.message });
+	} else if (error.name === 'JsonWebTokenError') {
+		return response.status(401).json({
+			error: 'invalid token',
+		});
+	} else if (error.name === 'TokenExpiredError') {
+		return response.status(401).json({
+			error: 'token expired',
+		});
 	}
 
 	next(error);
 };
 
+const tokenExtractor = (request, response, next) => {
+	let token;
+	const authorization = request.get('authorization');
+	if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+		request.token = authorization.substring(7);
+	} else {
+		request.token = null;
+	}
+	next();
+
+	return request.token;
+};
+
 module.exports = {
 	unknownEndpoint,
 	errorHandler,
+	tokenExtractor,
 };
